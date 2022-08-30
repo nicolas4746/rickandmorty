5087ce8667a6851b90818706aeda3c8b53c37d1d
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-30 14:06:07 -0300

    diseno final favoritos

--- src/components/CharacterCartFavorites.jsx
+++ src/components/CharacterCartFavorites.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import '../styles/characterCardFavorites.css'
 const CharacterCardFavorites = ({image, name,species,handleClickRemove,id}) => {
     return (
-        <div className="card">
-            <figure className="img-container">
+        <div className="card-fav">
+            <figure className="img-container-fav">
                 <img src={image} alt={name}/>
             </figure>
-            <article className="text-container">
+            <article className="text-container-fav">
                 <h2>{name}</h2>
-                <p>{species}</p>
                 <button className="btn-del-fav" type="button" onClick={()=>handleClickRemove(id)}>Eliminar de favoritos</button>
             </article>
         </div>
