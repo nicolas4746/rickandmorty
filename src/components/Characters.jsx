e6218587992730c320c33924395385b7b5702d24
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-31 11:24:05 -0300

    creando custom hooks para consumo de api

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,13 +1,15 @@
-import React, {useState, useEffect, useReducer, useMemo, useRef, useCallback} from 'react'
+import React, {useState, useReducer, useMemo, useRef, useCallback} from 'react'
 import CharacterCard from './CharacterCard';
 import CharacterCardFavorites from './CharacterCartFavorites';
 import '../styles/character.css'
 import CharacterNotFound from './CharacterNotFound';
 import Search from './Search';
+import useCharacters from '../hooks/useCharacters';
 
 const initialState = {
     favorites: [],
 }
+const API = 'https://rickandmortyapi.com/api/character/'
 
 const favoriteReducer = (state, action) => {
     switch (action.type) {
@@ -31,23 +33,14 @@ const favoriteReducer = (state, action) => {
 }
 
 const Characters = () => {   
-    const [characters, setCharacters] = useState([]);
     const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
     const[search,setSearch] = useState('');
     const searchInput = useRef(null);
 
-   /*  const handleSearch = () => {
-        setSearch(searchInput.current.value)
-    } */
+    const characters = useCharacters(API);
+
     const handleSearch = useCallback( ()=>{
         setSearch(searchInput.current.value)
-    }, []);
-
-
-    useEffect(() =>{
-        fetch('https://rickandmortyapi.com/api/character/')
-        .then(response => response.json())
-        .then(data => setCharacters(data.results))
     }, []);
 
     const handleClick = (favorite) => {
